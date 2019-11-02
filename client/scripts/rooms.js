baeff5447ca67bcd14f1580366bd3151c34a6c96
Aria Keshmiri <ariakesh@yahoo.com>
2019-11-02 11:30:03 -0700

    tried

--- client/scripts/rooms.js
+++ client/scripts/rooms.js
@@ -1,3 +1,10 @@
 var Rooms = {
-  
+    render: _.template(`
+
+      <div class="roomSelector">
+        <option value = "text"><%- rn %></option>
+      </div>
+
+    `)
+
 };
--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -4,23 +4,15 @@ var RoomsView = {
   $roomValue: $('#val'),
   $select: $('#rooms select'),
 
+
   initialize: function() {
     RoomsView.$button.on('click',function() {
-      RoomsView.renderRoom(RoomsView.$roomValue.value);
+      RoomsView.renderRoom(RoomsView.$roomValue.val);
+      console.log('is this working')
     });
   },
-
   renderRoom: function(name) {
-    RoomsView.$roomValue.prepend(RoomsView.render({rn: name}))
+    RoomsView.$select.prepend(Rooms.render({rn: roomname.name}))
+
   },
-
-  render: _.template(`
-
-      <div class="roomSelector">
-        <option class = "text" > <%- rn %></option>
-      </div>
-
-    `)
-
-
 };
